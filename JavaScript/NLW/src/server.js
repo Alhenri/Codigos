74bc8bc5e9aa2f1107478b935a42709c7ba8f743
Alhenri <ahssf@cin.ufpe.br>
2020-06-07 11:49:41 -0300

    Utilizando o nunjucks

--- JavaScript/NLW/src/server.js
+++ JavaScript/NLW/src/server.js
@@ -4,20 +4,31 @@ const server = express()
 //Configurar a leitura das pastas
 server.use(express.static("public"))
 
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {//onde ta os html, objeto
+    express: server,
+    noCache: true
+})
+
+
 //configurar caminhos da minha aplicação
     //pagina inicial
         //req: requisição
         //res: respostas
 server.get("/", (req, res) => { //get é um verbo de requisição http, e o '/' é a primeira chamada
-    res.sendFile(__dirname + "/views/index.html") //dirname é o diretório em que eu estou    
+    //res.sendFile(__dirname + "/views/index.html") //dirname é o diretório em que eu estou
+    return res.render("index.html", {
+        title: "Teste"
+    })
 })
 
-server.get("/create-point", (req, res) => {//o arquivo deverá ser chamado exatamente como está o primeiro argumento
-    res.sendFile(__dirname + "/views/create-point.html")
+server.get("/create", (req, res) => {//o arquivo deverá ser chamado exatamente como está o primeiro argumento
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 // ligar o servidor
 server.listen(3000)
